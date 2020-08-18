7cda42113d2c3de220bf9c89db0b272716d7fff0
Aaron Ponti <aarpon@gmail.com>
2020-08-18 13:35:29 +0200

    Add extract_POCT.py script and pypocquant.tools.extract_strip function to extract and return the strip region of an image.
    This can be used both from the command line or integrated in scripts or the UI.

---
+++ extract_POCT.py
@@ -0,0 +1,109 @@
+import argparse
+import cv2
+import rawpy
+from pathlib import Path
+import sys
+
+__exe__ = "extractPOCT"
+__version__ = "0.1.0"
+
+from pypocquant.lib.analysis import extract_rotated_strip_from_box, use_hough_transform_to_rotate_strip_if_needed
+from pypocquant.lib.barcode import try_extracting_fid_and_all_barcodes_with_linear_stretch_fh, rotate_if_needed_fh, \
+    align_box_with_image_border_fh, find_strip_box_from_barcode_data_fh
+from pypocquant.lib.processing import BGR2Gray
+from pypocquant.lib.tools import extract_strip
+
+if __name__ == '__main__':
+
+    #
+    # Parsing input arguments
+    #
+    parser = argparse.ArgumentParser(description='Extract POCT from selected image to help defining analysis paremeters.')
+
+    # Version
+    parser.add_argument(
+        '-v',
+        '--version',
+        action='version',
+        version=f"{__exe__} {__version__}"
+    )
+
+    # Input folder
+    parser.add_argument(
+        '-f',
+        '--filename',
+        default='',
+        help='image to be processed'
+    )
+
+    # Input folder
+    parser.add_argument(
+        '-o',
+        '--output_filename',
+        default='',
+        help="output file name"
+    )
+
+    parser.add_argument(
+        '-b-',
+        '--border',
+        default=40,
+        type=int,
+        help="Extension of the white border of the QR codes."
+    )
+
+    parser.add_argument(
+        '-r',
+        '--raw_auto_stretch',
+        action='store_true',
+        help="Apply auto-stretch of intensities for RAW images (only)."
+    )
+
+    parser.add_argument(
+        '-w',
+        '--raw_auto_wb',
+        action='store_true',
+        help="Apply auto white-balancing for RAW images (only)."
+    )
+
+    # Parse the arguments
+    args = vars(parser.parse_args())
+
+    # Extract the arguments
+    filename = args["filename"]
+    output_filename = args["output_filename"]
+    qr_code_border = args["border"]
+    raw_auto_stretch = args["raw_auto_stretch"]
+    raw_auto_wb = args["raw_auto_wb"]
+
+    # Check that the input file exists
+    if not Path(filename).is_file():
+        print(f"Error: The file {filename} does not exist!")
+        sys.exit(-1)
+
+    # Load  the image
+    if filename.lower().endswith(".jpg") or filename.lower().endswith(".jpeg"):
+        image = cv2.imread(filename)
+    elif filename.lower().endswith(".nef") or \
+            filename.lower().endswith(".cr2") or \
+            filename.lower().endswith(".arw"):
+        with rawpy.imread(filename) as raw:
+            image = raw.postprocess(
+                no_auto_bright=not raw_auto_stretch,
+                use_auto_wb=raw_auto_wb,
+                use_camera_wb=False,
+                gamma=(1, 1),
+                output_bps=8)
+
+        # Swap channels to BGR to be opencv compatible
+        image = image[:, :, [0, 1, 2]] = image[:, :, [2, 1, 0]]
+
+    # Run the extraction
+    strip_image, error_msg = extract_strip(image, qr_code_border)
+
+    if strip_image is None:
+        print(f"Sorry, extraction failed. {error_msg}.")
+    else:
+        # Save the final image
+        cv2.imwrite(output_filename, strip_image)
+        print(f"Successfully saved extracted POCT to {output_filename}.")
---
+++ lib/tools.py
@@ -0,0 +1,90 @@
+from pypocquant.lib.analysis import extract_rotated_strip_from_box, use_hough_transform_to_rotate_strip_if_needed
+from pypocquant.lib.barcode import try_extracting_fid_and_all_barcodes_with_linear_stretch_fh, rotate_if_needed_fh, \
+    align_box_with_image_border_fh, find_strip_box_from_barcode_data_fh
+from pypocquant.lib.processing import BGR2Gray
+
+
+def extract_strip(image, qr_code_border):
+    """Attempts to extract the strip from the original image.
+    @param image: RGB image.
+    @param qr_code_border: extension of the white border around the QR code.
+
+    @return (strip, error_msg): Tuple
+        strip: Strip image (RGB) or None if extraction fails.
+        error_msg: If strip is None, the cause of failure will be stored in error_message.
+    """
+
+    # Find the location of the barcodes
+    barcode_data, _, _, _, _, _, _, _, best_score, _ = \
+        try_extracting_fid_and_all_barcodes_with_linear_stretch_fh(
+            image,
+            lower_bound_range=(0, 5, 15, 25, 35),
+            upper_bound_range=(100, 98, 95, 92, 89),
+            scaling=(0.25, 0.5)
+        )
+
+    # Simple check -- if less than three QR codes were found, we have the guarantee that
+    # we do not have enough information to extract the strip box.
+    # @TODO: Actually check.
+    if best_score < 3:
+        return None, "Failed decoding QR codes to extract POCT position."
+
+    # Rotate the image if needed
+    image_was_rotated, image, image_log = rotate_if_needed_fh(
+        image,
+        barcode_data,
+        [],
+        verbose=False
+    )
+
+    # If the image was rotated, we need to find the location of the barcodes again.
+    # Curiously, re-using the best percentiles we found earlier is *not* guaranteed
+    # to succeed. Therefore, we search again.
+    # Also, if we managed to extract patient data last time and we fail this time
+    # we fall back to the previous values.
+    if image_was_rotated:
+        barcode_data, _, _, _, _, _, _, _, _, _ = \
+            try_extracting_fid_and_all_barcodes_with_linear_stretch_fh(
+                image,
+                lower_bound_range=(0, 5, 15, 25, 35),
+                upper_bound_range=(100, 98, 95, 92, 89),
+                scaling=(0.25, 0.5)
+            )
+
+    # Align the strip box with the image borders (remove potential rotation)
+    image, box_rotation_angle = align_box_with_image_border_fh(barcode_data, image)
+
+    # Find location of the strip box from the barcode data
+    box, qr_code_extents, qc_image, box_rect = find_strip_box_from_barcode_data_fh(
+        image,
+        barcode_data,
+        qr_code_border=qr_code_border,
+        qc=False)
+
+    if box is None or box.shape[0] == 0 or box.shape[1] == 0:
+        return None, "Could not extract POCT from barcode data."
+
+    # Convert box to gray value
+    box_gray = BGR2Gray(box)
+
+    # Crop the strip from the box while correcting for possible rotation
+    strip_gray, strip = extract_rotated_strip_from_box(box_gray, box)
+
+    # Make a copy of the strip images for analysis
+    strip_for_analysis = strip.copy()
+    strip_gray_for_analysis = strip_gray.copy()
+
+    # Since the strip is sometimes placed facing the wrong direction
+    # in the box, we will try a couple of approaches to determine
+    # whether we should rotate it 180 degrees.
+
+    # Use the Hough transform to look for expected details in the
+    # strip.
+    _, strip_for_analysis, _, _ = \
+        use_hough_transform_to_rotate_strip_if_needed(
+            strip_gray_for_analysis,
+            strip_for_analysis,
+            qc=False
+        )
+
+    return strip_for_analysis, ""
