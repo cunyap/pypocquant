57a6580bca99fb09b5ed9328af2d2315f048067a
Aaron Ponti <aarpon@gmail.com>
2021-01-06 09:50:23 +0100

    Remove hard-coded path.

--- helpers/__init__.py
+++ helpers/__init__.py
@@ -1,6 +1,3 @@
-#!/bin/bash
-
-#
 #  ********************************************************************************
 #   Copyright © 2020-2021, ETH Zurich, D-BSSE, Andreas P. Cuny & Aaron Ponti
 #   All rights reserved. This program and the accompanying materials
@@ -12,8 +9,4 @@
 #     * Andreas P. Cuny - initial API and implementation
 #     * Aaron Ponti - initial API and implementation
 #  *******************************************************************************
-#
-INPUT_ROOT_FOLDER=__SET_YOUR_INPUT_FOLDER__
-OUTPUT_ROOT_FOLDER=${INPUT_ROOT_FOLDER}
 
-python collect_results.py -f ${INPUT_ROOT_FOLDER} -o ${OUTPUT_ROOT_FOLDER}
