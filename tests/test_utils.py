5a8d5833ad864c088a96c96168d5c0b32340aeb7
Andreas P. Cuny <andreas.cuny@bsse.ethz.ch>
2021-01-12 16:23:55 +0100

    Update unit test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -11,15 +11,44 @@
 #  *********************************************************************************
 
 from unittest import TestCase, main
-from pypocquant.lib.utils import get_iso_date_from_image, get_exif_details
+from pypocquant.lib.utils import get_iso_date_from_image, get_exif_details, get_orientation_from_image, is_on_path, \
+    get_project_root, create_quality_control_images, image_format_converter
 import numpy as np
 from pathlib import Path
-
-# from pypocquant.lib.io import load_and_process_image
-# image = load_and_process_image(full_filename, raw_auto_stretch=False, raw_auto_wb=False, to_rgb=False)
+import pytesseract
+from pypocquant.lib.io import load_and_process_image
 
 
 class TestUtils(TestCase):
+
+    def testCreateQualityControlImages(self):
+
+        file_path = Path(__file__).parent.absolute().parent
+        full_filename = str(Path(file_path / 'examples' / 'images' / 'IMG_9067.JPG'))
+        image = load_and_process_image(full_filename, raw_auto_stretch=False, raw_auto_wb=False, to_rgb=False)
+        images_dict = {'1': image}
+        create_quality_control_images(results_folder_path=Path(file_path / 'tests'), basename='qc_test_image',
+                                      map_of_images=images_dict, extension=".jpg", quality=100)
+
+        full_filename = str(Path(file_path / 'tests' / 'qc_test_image_1.jpg'))
+        image_new = load_and_process_image(full_filename, raw_auto_stretch=False, raw_auto_wb=False, to_rgb=False)
+        self.assertEqual(image.all(), image_new.all())
+
+    def testImageFormatConverter(self):
+
+        file_path = Path(__file__).parent.absolute().parent
+        image_format_converter(Path(file_path / 'tests' / 'test_raw'), filename='DSC_0115.nef',
+                               output_dir=Path(file_path / 'tests' / 'test_raw'), image_format='jpeg')
+
+        full_filename = str(Path(file_path / 'tests' / 'test_raw' / 'DSC_0115.jpeg'))
+        image_new = load_and_process_image(full_filename, raw_auto_stretch=False, raw_auto_wb=False, to_rgb=False)
+        self.assertEqual((2868, 4310, 3), image_new.shape)
+
+    def testGetProjectRoot(self):
+        file_path = Path(__file__).parent.absolute().parent
+
+        ret = get_project_root()
+        self.assertEqual(file_path, ret)
 
     def testGetIsoDateFromImage(self):
         file_path = Path(__file__).parent.absolute().parent
@@ -36,11 +65,26 @@ class TestUtils(TestCase):
         full_filename = str(Path(file_path / 'examples' / 'images' / 'IMG_9067.JPG'))
 
         exp_time, f_number, focal_length_35_mm, iso_speed = get_exif_details(full_filename)
-        print(exp_time, f_number, focal_length_35_mm, iso_speed)
 
-        # print(f"\nExpected result: Iso time: [1/10] ; Test result: {exp_time}")
-        print([1/10], np.array(exp_time[0]))
-        # self.assertAlmostEquals([1/10], np.array(exp_time[0]), places=4)
+        self.assertAlmostEqual(1/10, np.array(exp_time[0].num/exp_time[0].den), places=4)
+        self.assertAlmostEqual(63 / 10, np.array(f_number[0].num / f_number[0].den), places=4)
+        self.assertEqual(-1, focal_length_35_mm)
+        self.assertEqual(100, np.array(iso_speed[0]))
+
+    def testGetOrientationFromImage(self):
+        file_path = Path(__file__).parent.absolute().parent
+        full_filename = str(Path(file_path / 'examples' / 'images' / 'IMG_9067.JPG'))
+
+        orientation = get_orientation_from_image(full_filename)
+        self.assertEqual('Horizontal (normal)', orientation)
+
+    def testIsOnPath(self):
+
+        ret = is_on_path('tesseract')
+        self.assertEqual(False, ret)
+
+    def testSetTesseract(self):
+        print(pytesseract.pytesseract.tesseract_cmd)
 
 
 if __name__ == "__main__":
